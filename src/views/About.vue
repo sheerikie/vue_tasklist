52f89c3653ec8f4b0785e6181d9f323117ff28ff
sheerikie <sheerikie@gmail.com>
2019-06-14 10:15:13 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Add Any Task You Want In the Application</p>
+  </div>
+</template>
+<style scoped>
+  .about {
+    background: green;
+    color: gray;
+    text-align: center;
+    
+  }
+
+  
+</style>
